874992320af4ef6f012459ea2a92896bb9027bf8
Reyan Tropia <reyan2323@gmail.com>
2017-04-14 14:35:56 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div id="header">
+        <div id="prof-pic"></div>
+        <div id="greet">Reyan Tropia</div>
+        <div id="greet-msg">Eat-Code-Repeat</div>
+    </div>
+</body>
+</html>
